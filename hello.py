cc8f1b44b8e80ebad657cae173513c47da095f63
cmis <cmis@cmis-hs14.(none)>
2016-01-27 11:13:55 +0700

    Created hello.py. Wrote a few lines of print.

---
+++ hello.py
@@ -0,0 +1,15 @@
+
+print "Hello, world"
+print "I'm no one"
+print "who are you?"
+print True
+print False
+print 01231231
+print 711298712
+print -1234218
+print 0.021
+print 0.12312
+print 912.124324
+print (True, "ABD", "ISUADGS", 0.01113, 8712321)
+print (False, 8.23123)
+print ("ADSASADASASDSAD", "WQEWQEWQEWQEWQ", 12345678, False)
