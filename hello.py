9241fa90b62555b56a5b645125e5db69adfe4f99
cmis <cmis@cmis-hs14.(none)>
2016-01-28 14:34:31 +0700

    Edited hello.py. Added a few variables and their definitions.

--- hello.py
+++ hello.py
@@ -1,4 +1,11 @@
-
+myName = "Walter"
+myIGN = "armorinfinity"
+myAge = 19
+myGrade = 11
+myPhoneModel = "iPhone 5S"
+myFavouriteClass = "Computer Science"
+blah = "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah"
+SMOrc = "Face never trade"
 print "Hello, world"
 print "I'm no one"
 print "who are you?"
@@ -13,3 +20,6 @@ print 912.124324
 print (True, "ABD", "ISUADGS", 0.01113, 8712321)
 print (False, 8.23123)
 print ("ADSASADASASDSAD", "WQEWQEWQEWQEWQ", 12345678, False)
+print myName, myIGN, myAge, myGrade, myPhoneModel, myFavouriteClass
+print blah
+print SMOrc
