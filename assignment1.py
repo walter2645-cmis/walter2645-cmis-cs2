4d947f62c1b9e3decc67bb2b70aba4a8ab5faee6
cmis <cmis@cmis-hs14.(none)>
2016-02-01 09:57:15 +0700

    Created assignment1.py. Defined all variables and started printing first message.

---
+++ assignment1.py
@@ -0,0 +1,14 @@
+myName = "Walter"
+myAgeinYears = 19.1
+myHeightinMeters = 1.73
+sideofsquare = 1
+lengthofrectangle = 2
+heightofrectangle = 3
+myAgeinMonths = myAgeinYears*12
+yearsbeforedeath = 71-myAgeinYears
+myHeightinFeet = myHeightinMeters*3.28084
+diffFromAverageHeight = 1.734-myHeightinMeters
+areaofsquare = sideofsquare*sideofsquare
+halfvolumeofcube = areaofsquare*sideofsquare/2.0
+oneNinthofRectangleArea = lengthofrectangle*heightofrectangle/9.0
+print "My name is " + myName + ". I am "
