684c167f9c0bc978e1a9c3286bd71a222f33ff91
cmis <cmis@cmis-hs14.(none)>
2016-02-09 14:05:36 +0700

    Created practisefunctions.py. Put practise section of functions.py here.

---
+++ practisefunctions.py
@@ -0,0 +1,22 @@
+def greet():
+    print "Hello"
+    print "How are you?"
+    print "Will you give me 1 trillion bananas?"
+    print "Thank you."
+    print "Good bye!"
+greet()
+def insult():
+    print "Hello"
+    print "Yo momma so fat they thought she was a new planet."
+    print "You smell terrible."
+    print "Good day!"
+insult()
+def compliment(name):
+    print "Hello,", name
+    print "How are you", name , "?"
+    print "You look good today", name , "."
+    print "Good Bye!"
+compliment("abc")
+def error(a, b, c):
+    print c,a,b
+error("Syntax", "Runtime", "Semantic")
