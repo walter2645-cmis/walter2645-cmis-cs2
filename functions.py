babc1573fb6a7d4b750b4ba45423765aea550adc
cmis <cmis@cmis-hs14.(none)>
2016-02-09 14:04:57 +0700

    Edited functions.py. Removed practise section.

--- functions.py
+++ functions.py
@@ -1,26 +1,4 @@
 import math
-def greet():
-    print "Hello"
-    print "How are you?"
-    print "Will you give me 1 trillion bananas?"
-    print "Thank you."
-    print "Good bye!"
-greet()
-def insult():
-    print "Hello"
-    print "Yo momma so fat they thought she was a new planet."
-    print "You smell terrible."
-    print "Good day!"
-insult()
-def compliment(name):
-    print "Hello,", name
-    print "How are you", name , "?"
-    print "You look good today", name , "."
-    print "Good Bye!"
-compliment("abc")
-def error(a, b, c):
-    print c,a,b
-error("Syntax", "Runtime", "Semantic")
 def add(a, b):
     return a + b
 def sub(a, b):
