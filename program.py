b499b62ac18fa5620ad28a9a9a4578aa1f772014
cmis <cmis@cmis-hs14.(none)>
2016-02-11 09:55:54 +0700

    Edited program.py. Wrote two short functions that used raw input to perfom a calculation.

--- program.py
+++ program.py
@@ -1,6 +1,11 @@
 import math
-def heightdifference():
-    Fheight = int(raw_input("How many centimeters tall is your father?\n"))
-    height = int(raw_input("How many centimeters tall are you?\n"))
-    return "The difference in your heights is " + str(Fheight - height) + " centimeters."
-print heightdifference()
+def circlecircumference():
+    radius = int(raw_input("How many units long is the radius?\n"))
+    unit = raw_input("What is the unit of measurment?\n")
+    return "The measurement of the circumference of the circle is " + str(2 * math.pi * radius) + unit + "(s)."
+def hypotenuseofrighttriangle():
+    x = int(raw_input("How many units long is 1 side of the right triangle?\n"))
+    y = int(raw_input("How many units long is the other side of the right triangle?\n"))
+    unit = raw_input("What is the unit of measurement?\n")
+    return "The hypotenuse of the right triangle is " + str(math.sqrt((x * x) + (y * y))) + unit + "(s) long."
+print hypotenuseofrighttriangle()
