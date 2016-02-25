8fc4bb224d9b8ca2fd35518db6c86e4ad0cc71c3
cmis <cmis@cmis-hs14.(none)>
2016-02-25 09:49:27 +0700

    Edited test.py. Tested some boolean expressions and stuff in functions.

--- test.py
+++ test.py
@@ -1,10 +1,22 @@
-name = raw_input("What is your name?\n")
-a = int(raw_input("Type a number:\n"))
-b = int(raw_input("Type another number:\n"))
-c = a + b
-abc = """
-Greetings {}.
-Did you know:
-{} + {} = {}
-""".format(name, a, b, c)
-print abc
+def is_divisible(x, y):
+    if x % y == 0:
+        return True
+    else:
+        return False
+def text_comp():
+    a = raw_input("Type anything:\n")
+    b = raw_input("Type exactly what you typed just now again, please:\n")
+    if a == b:
+        return "Congratulations! You followed the instructions correctly!"
+    else:
+        return "sdfk"
+def num_comp():
+    a = float(raw_input("Type a number:\n"))
+    b = float(raw_input("Type another number:\n"))
+    if a == b:
+        return "The numbers are equal."
+    elif a > b:
+        return "The first number is greater than the second number."
+    elif a < b:
+        return "The first number is less than the second number."
+print num_comp()
