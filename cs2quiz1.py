eb2cd2c82f8d2eb5b45e9c3bd6dbc7982c71ed94
cmis <cmis@cmis-hs14.(none)>
2016-03-03 11:55:41 +0700

    Marked quiz.

--- cs2quiz1.py
+++ cs2quiz1.py
@@ -1,15 +1,15 @@
 #Part 1: Terminology (15 points)
 #1 1pt) What is the symbol "=" used for?
 # assigning values, function calls to a variable
-#
+# 1 pt right answer
 #
 #2 3pts) Write a technical definition for 'function'
 # A function is a named sequence of statements that perform some calculation and may return an output
-#
+# 3 pts right answer
 #
 #3 1pt) What does the keyword "return" do?
 # returns some form of output from a function
-#
+# 1 pt right answer
 #
 #4 5pts) We know 5 basic data types. Write the name for each one and provide two
 #   examples of each below
@@ -18,19 +18,19 @@
 #	3: float: 1.23, 14264.80124
 #	4: integer: 314, 0
 #	5: tuple: True, "SDA", 12.456, 87
-#
+# 4pts, missed parenthesis on tuple
 #5 2pts) What is the difference between a "function definition" and a 
 #        "function call"?
 # a function definition defines what the function does and a function call calls the function to do what it was defined to do.
 # the main difference between the two is the definition has a ":" after it while the function call does not
 # a function must me defined before it can be called
-#
+#  1.5 pts, mostly right answer, missed function name
 #6 3pts) What are the 3 phases that every computer program has? What happens in
 #        each of them
 #	1: Input: user inputs something
 #	2: Processing/computation: computer does something with the input
 #	3: Output: computer returns some form of output
-#
+# 3pts right answer
 #Part 2: Programming (25 points)
 #Write a program that asks the user for the areas of 3 circles.
 #It should then calculate the diameter of each and the sum of the diameters 
@@ -45,8 +45,18 @@
 
 # Hint: Radius is the square root of the area divided by pi
 import math
+#1 pt for header line 1 pt
+#3 pt for correct formula 1 pt
+#1 pt for return value 1 pt
+#1 pt for parameter name 0 pt put x instead of area
+#1 pt for function name 1 pt
 def diameterfromarea(x):
     return math.sqrt(x/math.pi)*2
+#1pt for header line 1 pt
+#1pt for parameter names 1 pt
+#1pt for return value 1 pt
+#1pt for correct output format 1 pt
+#3pt for correct use of format function 1 3 pts
 def output(c1, c2, c3, total):
     out = """
 Circle  Diameter
@@ -56,6 +66,12 @@ c3      {}
 Totals  {}
 """.format(c1, c2, c3, total)
     return out
+#1pt header line 1 pt
+#1pt getting input 1 pt
+#1pt converting input 1 pt
+#1pt for calling output function 1 pt
+#2pt for correct diameter formula 2 pts
+#1pt for variable names 0 pt
 def main():
     #Input Section
     a = float(raw_input("Area of C1: "))
@@ -71,4 +87,8 @@ def main():
     #Output Section
     res = output(c1, c2, c3, total)
     print res
+#1pt for calling main 1 pt
 main()
+#1pt explanatory comments 1 pt
+#1pt code format 1 pt
+
