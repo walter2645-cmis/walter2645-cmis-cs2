3c6ea436a54fcde61d0fce727d79c8d29efc1516
cmis <cmis@cmis-hs14.(none)>
2016-03-09 11:54:34 +0700

    Created choose.py. Started conditional execution assignment.

---
+++ choose.py
@@ -0,0 +1,7 @@
+print "Greetings Traveler."
+#You are playing an antique RPG game.
+#You are asked for your age. returns a different message depending on whether player is over 18.
+#You are asked if you want to read an introduction to the game.
+#If you choose yes, it will print out a brief introduction.
+#You are asked if you wish your character to be a mage or a warrior.
+#
