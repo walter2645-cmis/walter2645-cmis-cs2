914f0906b9608c27d84d2c03990c7063d1c5696e
cmis <cmis@cmis-hs21.(none)>
2016-03-22 09:57:33 +0700

    Edited conditionals.py. Added some ideas for printer and mysterybox.

--- conditionals.py
+++ conditionals.py
@@ -2,10 +2,26 @@
 #You are asked for your age. returns a message depending on whether player is over 18.
 #You are asked if you want to read an introduction to the game.
 #If you choose yes, it will print out a brief introduction.
-#
+#You encounter a maze. You are given an option to attempt it or do not.
+#If you attempt it, it will loop back to the start of the maze until you choose not to attempt it.
 #
 def ansprocess(answer):
     return bool(answer)
+def printer(printorder):
+    if printorder == 1:
+        print
+    elif printorder == 2:
+        print
+    elif printorder == 3:
+        print
+    elif printorder == 4:
+        print
+def mysterybox():
+    if random.random == random.random:
+        print "You are officially the luckiest person that ever existed. You are granted 3 wishes."
+        #wishes function?
+    else:
+        print "The box instantly vanishes as you try to open it."
 def legalage(legal):
     if ansprocess(legal):
         print "Warning:\nYou will be psychologically scarred if you continue."
@@ -15,7 +31,7 @@ def introduction(intro):
 def mazeloop(maze):
     if ansprocess(maze):
         print "You wander the maze for hours, eventually stumbling onto a teleporter that returns you to the start of the maze."
-        maze = raw_input("Do you wish to attempt the maze again? ")
+        maze = raw_input("\nDo you wish to attempt the maze again? ")
         mazeloop(maze)
     else:
         print "You decide to go around the maze instead of going through it."
@@ -29,6 +45,7 @@ def main():
     print "You encounter a large maze. There seems to be some reward if you pass through it."
     maze = raw_input("Do you wish to attempt the maze or not? ")
     mazeloop(maze)
+    print "\nA mysterious box with question marks carved all over its surface appears in front of you. There is a note on top of it. It says: \"This mystery box contains three wishes. However, there is only a 1 in a trillion chance it will not disappear when you open it.\""
     #Processing
     
     #Output
