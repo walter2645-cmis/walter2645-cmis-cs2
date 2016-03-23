90543cb543ea0bd824dbf7d0ffe80e13dfb6aeb8
cmis <cmis@cmis-hs21.(none)>
2016-03-23 11:56:21 +0700

    Edited hypotenuse and circumference.py. Changed cariable names to be more descriptive.

--- hypotenuse and circumference.py
+++ hypotenuse and circumference.py
@@ -3,26 +3,26 @@ def hypotenuse(x, y):
     return math.sqrt((x ** 2) + (y ** 2))
 def circumference(a):
     return a ** 2 * math.pi
-def output(name, x, y, z, a, b):
+def output(name, triangleside1, triangleside2, hypresult, circleradius, circumferenceresult):
     out = """
 Greetings, {}.
 Did you know:
 The length of the hypotenuse of a right triangle with side lengths {} and {} is {}
 The circumference of a circle with radius {} is {}.
-""".format(name, x, y, z, a, b)
+""".format(name, triangleside1, triangleside2, hypresult, circleradius, circumferenceresult)
     return out
 def main():
     #Input Section
     name = raw_input("What is your name?\n")
-    x = int(raw_input("Type a number\n"))
-    y = int(raw_input("Type another number\n"))
-    a = int(raw_input("Type another number\n"))
+    triangleside1 = int(raw_input("Type a number\n"))
+    triangleside2 = int(raw_input("Type another number\n"))
+    circleradius = int(raw_input("Type another number\n"))
 
     #Processings
-    z = hypotenuse(x, y)
-    b = circumference(a)
+    hypresult = hypotenuse(triangleside1, triangleside2)
+    circumferenceresult = circumference(circleradius)
 
     #Output Section
-    res = output(name, x, y, z, a, b)
+    res = output(name, triangleside1, triangleside2, hypresult, circleradius, circumferenceresult)
     print res
 main()
