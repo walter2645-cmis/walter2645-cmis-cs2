64becef94b050e840f2ad3430a223f50da4d02ae
cmis <cmis@cmis-hs21.(none)>
2016-05-06 11:56:24 +0700

    Edited newguess2.py. Did guessing part.

--- newguess2.py
+++ newguess2.py
@@ -1,8 +1,15 @@
-def compguess():
+def compguess(bot, top):
+    theguess = (top - bot) / 2 + bot
+    print "I guess " + str(theguess) + "."
     judgement = raw_input("too (h)igh, too (l)ow, or (c)orrect: ")
     if judgement == "c":
         return "I'm awesome!"
     elif judgement == "h":
-        compguess()
+        top = theguess
+        return compguess(bot, top)
     elif judgement == "l":
-        compguess()
+        bot = theguess
+        return compguess(bot, top)
+compguess(1, 100)
+def main():
+    cres = compguess(1, 100)
