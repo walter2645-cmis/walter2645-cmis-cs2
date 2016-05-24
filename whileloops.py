9f12e14aa376e2f7716e6f6531e37a38b943ad5b
cmis <cmis@cmis-hs21.(none)>
2016-05-24 09:55:41 +0700

    Created whileloops.py. Wrote counter functions.

---
+++ whileloops.py
@@ -0,0 +1,16 @@
+def counter(x):
+    if x > 0:
+        while x >= 0:
+            print x
+            x -= 1
+    else:
+        while x <= 0:
+            print x
+            x += 1
+def counter2(x):
+    while x >= 0:
+        print x
+        x -= 1
+    while x <= 0
+        print x
+        x += 1
